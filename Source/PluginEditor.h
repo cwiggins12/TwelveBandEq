17eb70ed84ac2279ab33e89fa202620ee6aebadf
cwiggins12 <codywiggins2112@gmail.com>
2025-09-05 20:26:49 -0500

    finalized edge cases of buttons

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd98bc6fefd00d3ff7484d1000bbbe92ee8994e22745258f8cf0e3ff14942190
-size 4575
+oid sha256:d72faa5f5d35f3c2fcdc70b9736b2b526b61485be30bd29be5ffd1cd68fbd61e
+size 4417
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4f89c19e548fb1cd73775cbe2f384e920f64719525d50088edf89a9b408d210
-size 10161
+oid sha256:632cfd06ab1b0018b44319ea7c74288236d38a72338320a5bc45d8066d9feb95
+size 9888
